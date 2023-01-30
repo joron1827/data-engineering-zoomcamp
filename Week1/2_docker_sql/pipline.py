dabdb99842e6742e1d96a48b7aeb107d9699a729
joron1827 <61901261+joron1827@users.noreply.github.com>
2023-01-30 19:13:50 +0900

    update ingest.py

--- Week1/2_docker_sql/pipline.py
+++ Week1/2_docker_sql/pipline.py
@@ -1,5 +1,12 @@
-import pandas as pod
+
+import sys
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
 
 # some fancy stuff with pandas
 
-print('job finished successfully')
+print(f'job finished successfully for day = {day}')
